f76c96cca8e25e8da4ab0ba57f3c5be7fea4ede3
Rajesh Koilpillai <rajesh.koilpillai@gmail.com>
2012-11-23 11:50:24 +0530

    cap sidekiq integration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sidekiq::Application.config.secret_token = 'b8476ba8169deb2aec0ac5330f818ee6c5a830e24aa9e6ad48ec59bfbb43c765bacf2b1941c546b1684039f478d7fa7cbed51be863b49bbe6fd209d61c8eec3c'
